71365b940181a23ad6cfa7e299a85b4552def7c9
D4rkJvck <d4rkjvck@gmail.com>
2024-05-03 09:40:10 +0000

    Chapter 9 [DONE]

---
+++ errors/src/main.rs
@@ -0,0 +1,71 @@
+use std::fs::File;
+use std::io::{self, ErrorKind, Read};
+
+fn main() { // -> Result<(), Box<dyn Error>> {
+    match read_file("hello.txt") {
+        Ok(content) => {
+            println!("\nFile content:\n{:?}", content);
+
+            let last = last_char_of_first_line(&content);
+            println!("\nLast Character of first line: {:?}", last);
+        }
+        Err(error) => panic!("\nProblem reading content:\n{:?}", error),
+    };
+}
+
+////////////////////////////////////////////////////////////////////////
+
+fn read_file(path: &str) -> Result<String, io::Error> {
+    println!("\nReading file...");
+
+    let mut content = String::new();
+    
+    open_file(path)?.read_to_string(&mut content)?;
+    Ok(content)
+}
+
+//----------------------------------------------------------
+
+fn open_file(path: &str) -> Result<File, io::Error> {
+    println!("\nOpening file...");
+
+    match File::open(path) {
+        Ok(file) => {
+            println!("\n{:#?}", file);
+            Ok(file)
+        }
+        Err(error) => {
+            println!("\nProblem opening file:\n{:?}", error);
+            match error.kind() {
+                ErrorKind::NotFound => {
+                    create_file(path)?;
+                    open_file(path)
+                },
+                _ => Err(error),
+            }
+        }
+    }
+}
+
+//----------------------------------------------------------
+
+fn create_file(path: &str) -> Result<File, io::Error> {
+    println!("\nCreating file...");
+    
+    match File::create(path) {
+        Ok(file) => {
+            println!("\n{:#?}", file);
+            Ok(file)
+        }
+        Err(error) => {
+            println!("\nProblem creating file:\n{:?}", error);
+            Err(error)
+        }
+    }
+}
+
+//----------------------------------------------------------
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,27 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+// struct Guess {
+//     value: i32,
+// }
+
+// impl Guess {
+//     fn new(value: i32) -> Guess {
+//         if value < 1 || value > 100 {
+//             panic!("Guess value must be between 1 and 100, got {:?}.", value);
+//         }
+
+//         Guess { value }
+//     }
+
+//     fn value(&self) -> i32 {
+//         self.value
+//     }
+// }
+
 fn main() {
     println!("Guess the number!");
-    
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
@@ -15,12 +33,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("Fail to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100!");
+            continue;
+        }
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
